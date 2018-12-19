d1c147b41586a0a9e4066aa2fe479036e702548b
Felice Forby <feliceforby@gmail.com>
2018-12-19 12:39:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,5 +23,7 @@ function calculatesFarePrice(startingBlock, endingBlock) {
 
   if (distance <= 400) {
     return 0;
+  } else if (distance > 400 && distance <= 2000) {
+    return distance * 0.02;
   }
 }
