2b4ea46522ac9865043e4a9430bc99ef8e513ff9
Felice Forby <feliceforby@gmail.com>
2018-12-19 12:18:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 // Code your solution in this file!
-function distanceFromHqInBlocks() {
-  
+function distanceFromHqInBlocks(streetBlock) {
+  const hqBlock = 42;
+  return Math.abs(hqBlock - streetBlock);
 }
 
 function distanceFromHqInFeet() {
-  
+
 }
 
 function distanceTravelledInFeet() {
-  
+
 }
 
 function calculatesFarePrice() {
