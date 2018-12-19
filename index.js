5cd35adc75a97ac3c138fd1cddb0a98dd71bb911
Felice Forby <feliceforby@gmail.com>
2018-12-19 12:22:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solution in this file!
 function distanceFromHqInBlocks(streetBlockAtLocation) {
-  const blockAtHQ = 42;
-  return Math.abs(hqBlock - streetBlockAtLocation);
+  const blockAtHq = 42;
+  return Math.abs(blockAtHq - streetBlockAtLocation);
 }
 
 function distanceFromHqInFeet(streetBlockAtLocation) {
